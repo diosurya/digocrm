de0595a7f96aac700fb8496246d11e14ab6de638
Dio Putra <diosuryaputra95@gmail.com>
2026-05-22 14:20:30 +0700

    udpates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -32,7 +32,6 @@
                 }
             }
         </script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
         
         <style>
